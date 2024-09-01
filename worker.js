365b53d0e14f73d5610fc4a26044e8719ab3eb9f
muazmuhabau <muazmuhaba222@gmail.com>
2024-09-01 22:45:17 +0300

    Create worker.js

---
+++ worker.js
@@ -0,0 +1,16 @@
+if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker.register("/hamster-codes/service-worker.js").then(
+        function (registration) {
+          console.log(
+            "ServiceWorker registration successful with scope: ",
+            registration.scope
+          );
+        },
+        function (err) {
+          console.log("ServiceWorker registration failed: ", err);
+        }
+      );
+    });
+  }
+  
